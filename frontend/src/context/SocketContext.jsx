1c324456c25799ecce7b7efdae9f274c90383d6f
Kishan Kumar <121043195+Kishan2624@users.noreply.github.com>
2024-10-17 19:11:56 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   React.useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-1-sr13.onrender.com", {
         query: {
           userId: authUser.id,
         },
